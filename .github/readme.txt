deb7c9d6dd70b2cc102acd203f66c6ffa33e851a
Roadway000 <roadway000@gmail.com>
2024-05-18 22:41:59 +0300

    added .yml file for CI/CD

---
+++ .github/readme.txt
@@ -0,0 +1,5 @@
+1. Create directory ".github"
+2. Create directory "workflows"
+3. in directory "workflows" create "anyname.yml" file
+   as result should be ..\restapinote\.github\workflows\anyname.yml
+4.
--- src/test/java/com/example/restapinote/RestapinoteApplicationTests.java
+++ src/test/java/com/example/restapinote/RestapinoteApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.restapinote;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Slf4j
 @SpringBootTest
 class RestapinoteApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Hello!");
 	}
 
 }
